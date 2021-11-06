39e3db55b6df586849ae08d04f8327f8c31a7a91
KristopherPeck <Kristopher.R.Peck@gmail.com>
2021-11-06 09:56:53 -0400

    Update Export Registry Hives.ps1

--- Diagnostics/Export Registry Hives.ps1
+++ Diagnostics/Export Registry Hives.ps1
@@ -1,9 +1,16 @@
-#Create folder if it doesn't exist
-if (-NOT (Test-Path -path "C:\Export\")) {
-    New-Item -Path 'C:\' -Name "Export" -ItemType "directory"
-    }
+#Set the Path to where we want the exports to be stored.
+$path = "C:\Export\"
 
-#Run an export for each of the major hives except for Current User
-REG Export HKLM C:\OCS\$env:COMPUTERNAME-HKLM.reg
-REG Export HKU C:\OCS\$env:COMPUTERNAME-HKU.reg
-REG Export HKCR C:\OCS\$env:COMPUTERNAME-HKCR.reg
+#Check if the folder exists and create it if it does not.
+If(!(test-path $path))
+{
+      New-Item -ItemType Directory -Force -Path $path
+}
+
+#Run an export for each of the major hives except for Current User. It will overwrite any existing exports that were done. 
+Write-Host "Exporting HKLM"
+REG Export HKLM $path\$env:COMPUTERNAME-HKLM.reg /y
+Write-Host "Exporting HKU"
+REG Export HKU $path\$env:COMPUTERNAME-HKU.reg /y
+Write-Host "Exporting HKCR"
+REG Export HKCR $path\$env:COMPUTERNAME-HKCR.reg /y
