558a02c489b5c2911b64cdfb08f19926a2df2ffa
KristopherPeck <Kristopher.R.Peck@gmail.com>
2021-11-06 09:58:08 -0400

    Update Generate RAM Report.ps1
    Updated to use a variable for the path.

--- Diagnostics/Generate RAM Report.ps1
+++ Diagnostics/Generate RAM Report.ps1
@@ -1,9 +1,11 @@
-$folderName = "Export"
+#Set the Path to where we want the exports to be stored.
+$path = "C:\Export\"
 
-#Create export directory if it doesn't already exist
-if (-NOT (Test-Path -path "C:\$folderName\")) {
-  New-Item -Path 'C:\' -Name "$folderName" -ItemType "directory"
+#Check if the folder exists and create it if it does not.
+If(!(test-path $path))
+{
+      New-Item -ItemType Directory -Force -Path $path
 }
 
 #Use WMI to check information about installed RAM and then export it to a log file
-get-wmiobject win32_physicalmemory | Format-Table Manufacturer,Banklabel,Configuredclockspeed,Speed,Partnumber,Serialnumber -autosize | Out-File (New-Item -Path "C:\$folderName\$Env:COMPUTERNAME-Memory.log" -Force)
+get-wmiobject win32_physicalmemory | Format-Table Manufacturer,Banklabel,Configuredclockspeed,Speed,Partnumber,Serialnumber -autosize | Out-File (New-Item -Path "$path\$Env:COMPUTERNAME-Memory.log" -Force)
