5b1d490c388d7bc21d2e42eebae9a2ee4b9f9381
KristopherPeck <Kristopher.R.Peck@gmail.com>
2021-11-06 10:01:52 -0400

    Create CheckADPasswordExpirationDate.ps1

---
+++ Active Directory/CheckADPasswordExpirationDate.ps1
@@ -0,0 +1,10 @@
+#Set the Path to where we want the exports to be stored.
+$path = "C:\Export\"
+
+#Check if the folder exists and create it if it does not.
+If(!(test-path $path))
+{
+      New-Item -ItemType Directory -Force -Path $path
+}
+
+Get-ADUser -filter {Enabled -eq $True -and PasswordNeverExpires -eq $False} -Properties "DisplayName", "msDS-UserPasswordExpiryTimeComputed", "mail" |Select-Object -Property "Displayname", "mail",@{Name="ExpiryDate";Expression={[datetime]::FromFileTime($_.'msDS-UserPasswordExpiryTimeComputed')}} | Export-Csv -Path "$path\$Env:COMPUTERNAME-ADExpiredCreds.csv" -NoTypeInformation -Force
